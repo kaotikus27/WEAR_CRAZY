16207a92f9ee8224ea5285b644ccf800982c0b52
ELEOMAR <kaotikus27@gmail.com>
2024-10-19 10:00:01 +0800

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { AboutusComponent } from './components/pages/aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:DashboardComponent
+  },
+  {
+    path:'AboutUs',
+    component:AboutusComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/pages/navigation/navigation.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { AboutusComponent } from './components/pages/aboutus/aboutus.component';
+import { CollectionsComponent } from './components/pages/collections/collections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProductComponent,
+    DashboardComponent,
+    AboutusComponent,
+    CollectionsComponent
   ],
   imports: [
     BrowserModule,
